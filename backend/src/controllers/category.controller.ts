71c138d2a0c87bc46532bff7368afe2b6a0f711a
Azamgarh@1996 <sourabhsingh087@gmail.com>
2024-10-05 14:36:06 +0530

    code done

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
-import { addCategory, deleteCategory, getAllCategories, getCategoryById, getCategoryBySlug, updateCategory } from "../services/category.service";
+import {
+  addCategory,
+  deleteCategory,
+  getAllCategories,
+  getCategoryById,
+  getCategoryBySlug,
+  updateCategory,
+} from "../services/category.service";
 import { generateSlug } from "../shared/general.util";
 import { z } from "zod";
 import { User } from "../models/User";
@@ -7,132 +14,130 @@ import { deletePost, getAllPosts } from "../services/post.service";
 import { deletePostTagRelations } from "../services/post-tag.service";
 import { deletePostComments } from "../services/comment.service";
 
-
-
 export const getCategories = async (req: Request, res: Response) => {
-    // get user from req
-    const user = (req as any).user as User;
-    const categories = await getAllCategories({
-        userId: user.get('id')
-    });
-    console.log('categories', JSON.stringify(categories))
-    res.json(categories)
-}
+  // get user from req
+  const user = (req as any).user as User;
+  const categories = await getAllCategories({
+    userId: user?.get("id"),
+  });
+  console.log("categories", JSON.stringify(categories));
+  res.json(categories);
+};
 
-export const getCategoryBySlugController = async (req: Request, res: Response) => {
-    const slug = req.params.slug;
-    const category = await getCategoryBySlug(slug);
+export const getCategoryBySlugController = async (
+  req: Request,
+  res: Response
+) => {
+  const slug = req.params.slug;
+  const category = await getCategoryBySlug(slug);
 
-    if (!category) {
-        return res.status(404).json({ message: 'Category not found' })
-    }
+  if (!category) {
+    return res.status(404).json({ message: "Category not found" });
+  }
 
-    res.json(category)
-
-}
-
+  res.json(category);
+};
 
 export const addCategoryController = async (req: Request, res: Response) => {
-    const schema = z.object({
-        name: z.string()
-    });
+  const schema = z.object({
+    name: z.string(),
+  });
 
-    const user = (req as any).user as User
+  const user = (req as any).user as User;
 
-    const schemaValidator = schema.safeParse(req.body);
-    if (!schemaValidator.success) {
-        return res.status(400).json({ message: 'Invalid data', errors: schemaValidator.error })
-    }
+  const schemaValidator = schema.safeParse(req.body);
+  if (!schemaValidator.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid data", errors: schemaValidator.error });
+  }
 
-    const { name } = req.body;
-    const userId = user.get('id');
-    let slug = generateSlug(name);
+  const { name } = req.body;
+  const userId = user?.get("id");
+  let slug = generateSlug(name);
 
-    const categoryBySlug = await getCategoryBySlug(slug);
+  const categoryBySlug = await getCategoryBySlug(slug);
 
-    if (categoryBySlug) {
-        slug = generateSlug(name, true);
-    }
+  if (categoryBySlug) {
+    slug = generateSlug(name, true);
+  }
 
-    const category = await addCategory(name, slug, userId);
+  const category = await addCategory(name, slug, userId);
 
-    res.json(category)
-}
-
+  res.json(category);
+};
 
 export const updateCategoryController = async (req: Request, res: Response) => {
+  const schema = z.object({
+    name: z.string(),
+    id: z.number(),
+  });
 
-    const schema = z.object({
-        name: z.string(),
-        id: z.number()
-    });
+  const schemaValidator = schema.safeParse(req.body);
+  if (!schemaValidator.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid data", errors: schemaValidator.error });
+  }
 
-    const schemaValidator = schema.safeParse(req.body);
-    if (!schemaValidator.success) {
-        return res.status(400).json({ message: 'Invalid data', errors: schemaValidator.error })
-    }
+  let { name, id } = req.body;
 
-    let { name, id } = req.body;
+  let slug = generateSlug(name);
 
-    let slug = generateSlug(name);
+  const categoryBySlug = await getCategoryBySlug(slug);
 
-    const categoryBySlug = await getCategoryBySlug(slug);
+  if (categoryBySlug) {
+    res.status(400).json({ message: "Category already exists" });
+  }
 
-    if (categoryBySlug) {
-        res.status(400).json({ message: 'Category already exists' })
-    }
+  // check if category exist by the given id
+  let dbCategory = await getCategoryById(id);
 
-    // check if category exist by the given id
-    let dbCategory = await getCategoryById(id);
+  if (!dbCategory) {
+    res.status(404).json({ message: "Category not found" });
+  }
 
-    if (!dbCategory) {
-        res.status(404).json({ message: 'Category not found' })
-    }
+  // update the category
+  let updatedCategory = await updateCategory(name, slug, id);
 
-    // update the category
-    let updatedCategory = await updateCategory(name, slug, id);
-
-
-    res.json(updatedCategory);
-
-}
-
+  res.json(updatedCategory);
+};
 
 export const deleteCategoryController = async (req: Request, res: Response) => {
+  const schema = z.object({
+    id: z.number(),
+  });
 
-    const schema = z.object({
-        id: z.number()
-    });
+  const schemaValidator = schema.safeParse(req.body);
+  if (!schemaValidator.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid data", errors: schemaValidator.error });
+  }
 
-    const schemaValidator = schema.safeParse(req.body);
-    if (!schemaValidator.success) {
-        return res.status(400).json({ message: 'Invalid data', errors: schemaValidator.error })
-    }
+  const { id } = req.body;
 
-    const { id } = req.body;
+  const category = await getCategoryById(id);
+  if (!category) {
+    res.status(404).json({ message: "Category not found" });
+  }
 
-    const category = await getCategoryById(id);
-    if (!category) {
-        res.status(404).json({ message: 'Category not found' })
-    }
+  // getting all posts that belongs to the category
+  const posts = await getAllPosts({
+    categoryId: id,
+  });
 
-    // getting all posts that belongs to the category
-    const posts = await getAllPosts({
-        categoryId: id
-    });
+  const postIds = posts.map((post) => post.get("id"));
 
-    const postIds = posts.map(post => post.get('id'));
+  await deletePostTagRelations({
+    postId: postIds,
+  }); // delete post tag relations
 
+  await deletePostComments(postIds); // delete post comments
 
-    await deletePostTagRelations({
-        postId: postIds
-    }); // delete post tag relations
+  await deletePost(postIds); // delete posts
 
-    await deletePostComments(postIds); // delete post comments
+  await deleteCategory(id);
 
-    await deletePost(postIds); // delete posts
-
-    await deleteCategory(id);
-
-    res.json(category)
-}
+  res.json(category);
+};
--- backend/src/controllers/tag.controller.ts
+++ backend/src/controllers/tag.controller.ts
@@ -1,132 +1,139 @@
 import { Request, Response } from "express";
-import { addTag, deleteTag, getAllTags, getTagById, getTagBySlug } from "../services/tag.service";
+import {
+  addTag,
+  deleteTag,
+  getAllTags,
+  getTagById,
+  getTagBySlug,
+} from "../services/tag.service";
 import { z } from "zod";
 import { generateSlug } from "../shared/general.util";
 import { getPostById } from "../services/post.service";
-import { deletePostTagRelations, getPostTags } from "../services/post-tag.service";
+import {
+  deletePostTagRelations,
+  getPostTags,
+} from "../services/post-tag.service";
 import { User } from "../models/User";
 
 export const getTagsController = async (req: Request, res: Response) => {
-    // get user from req
-    const user = (req as any).user as User;
+  // get user from req
+  const user = (req as any).user as User;
 
   const tags = await getAllTags({
-    userId: user.get('id')
+    userId: user?.get("id"),
   });
   return res.json(tags);
 };
 
 export const addTagController = async (req: Request, res: Response) => {
+  const user = (req as any).user as User;
 
-    const user = (req as any).user as User
+  const schema = z.object({
+    name: z.string(),
+  });
 
-    const schema = z.object({
-        name: z.string()
-    });
+  const schemaValidator = schema.safeParse(req.body);
 
-    const schemaValidator = schema.safeParse(req.body);
+  if (!schemaValidator.success)
+    return res
+      .status(400)
+      .json({ message: "Invalid data", errors: schemaValidator.error });
 
-    if(!schemaValidator.success) return res.status(400).json({message: 'Invalid data', errors: schemaValidator.error})
-    
-    
-    const {name} = req.body;
+  const { name } = req.body;
 
-    let slug = generateSlug(name);
+  let slug = generateSlug(name);
 
-    const tagAlreadyExists = await getTagBySlug(slug);
+  const tagAlreadyExists = await getTagBySlug(slug);
 
-    if(tagAlreadyExists){
-        slug = generateSlug(name, true);
-    }
+  if (tagAlreadyExists) {
+    slug = generateSlug(name, true);
+  }
 
-    const newTag = await addTag(name, slug, user.get('id'));
+  const newTag = await addTag(name, slug, user?.get("id"));
 
-    return res.json(newTag);
-
-
+  return res.json(newTag);
 };
 
 export const updateTagController = async (req: Request, res: Response) => {
+  const schema = z.object({
+    name: z.string(),
+    id: z.number(),
+  });
 
-    const schema = z.object({
-        name: z.string(),
-        id: z.number()
-    })
+  const schemaValidator = schema.safeParse(req.body);
 
-    const schemaValidator = schema.safeParse(req.body);
+  if (!schemaValidator.success)
+    return res
+      .status(400)
+      .json({ message: "Invalid data", errors: schemaValidator.error });
 
-    if(!schemaValidator.success) 
-        return res.status(400).json({message: 'Invalid data', errors: schemaValidator.error})
+  const { name, id } = req.body;
 
+  const tag = await getTagById(id);
 
-    const {name, id} = req.body;
+  if (!tag) return res.status(404).json({ message: "Tag not found" });
+  if (tag.name === name)
+    return res.status(400).json({ message: "Nothing was changed." });
 
-    const tag = await getTagById(id);
+  let slug = generateSlug(name);
+  const tagAlreadyExists = await getTagBySlug(slug);
 
-    if(!tag) return res.status(404).json({message: 'Tag not found'});
-    if(tag.name === name) return res.status(400).json({message: 'Nothing was changed.'});
+  if (tagAlreadyExists) {
+    slug = generateSlug(name, true);
+  }
 
-    let slug = generateSlug(name);
-    const tagAlreadyExists = await getTagBySlug(slug);
+  tag.name = name;
+  tag.slug = slug;
+  await tag.save();
 
-    if(tagAlreadyExists){
-        slug = generateSlug(name, true);
-    }
-
-    tag.name = name;
-    tag.slug = slug;
-    await tag.save();
-
-    return res.json(tag);
-
-
-}
+  return res.json(tag);
+};
 
 export const deleteTagController = async (req: Request, res: Response) => {
+  const schema = z.object({
+    id: z.number(),
+  });
 
-    const schema = z.object({
-        id: z.number()
-    });
+  const schemaValidator = schema.safeParse(req.body);
 
-    const schemaValidator = schema.safeParse(req.body);
+  if (!schemaValidator.success)
+    return res
+      .status(400)
+      .json({ message: "Invalid data", errors: schemaValidator.error });
 
-    if(!schemaValidator.success) return res.status(400).json({message: 'Invalid data', errors: schemaValidator.error})
+  const { id } = req.body;
+  const tag = await getTagById(id);
 
-    const {id} = req.body;
-    const tag = await getTagById(id);
+  if (!tag) return res.status(404).json({ message: "Tag not found" });
+  await deletePostTagRelations({ tagId: id });
 
-    if(!tag) return res.status(404).json({message: 'Tag not found'});
-    await deletePostTagRelations({tagId: id});
+  await deleteTag(id);
 
-    await deleteTag(id);
-
-    return res.json(tag);
-}
-
+  return res.json(tag);
+};
 
 export const getPostTagsController = async (req: Request, res: Response) => {
-    let postId: any = req.params.postId;
+  let postId: any = req.params.postId;
 
-    postId = parseInt(postId);
+  postId = parseInt(postId);
 
-    if(!postId) return res.status(400).json({message: 'Post id is required'});
+  if (!postId) return res.status(400).json({ message: "Post id is required" });
 
-    const post = await getPostById(postId);
+  const post = await getPostById(postId);
 
-    if(!post) return res.status(404).json({message: 'Post not found'});
+  if (!post) return res.status(404).json({ message: "Post not found" });
 
-    const postTags = await getPostTags(postId);
+  const postTags = await getPostTags(postId);
 
-    return res.json(postTags);
-}
-
+  return res.json(postTags);
+};
 
 export const getTagBySlugController = async (req: Request, res: Response) => {
-    const {slug}  = req.params;
+  const { slug } = req.params;
 
-    const tag = await getTagBySlug(slug);
+  const tag = await getTagBySlug(slug);
 
-    if(!tag) return res.status(404).json({message: 'Tag not found'});
+  if (!tag) return res.status(404).json({ message: "Tag not found" });
 
-    return res.json(tag);
-}
+  return res.json(tag);
+};
